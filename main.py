964206dba03260741f09ea346fa30d32bf71de8b
Bastien Patureau <71067765+bpatureau@users.noreply.github.com>
2023-11-09 15:06:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 def main():
 # définition globales
 
-    food_stock = 10000
+    food_stock = 10000000
     queen_nbr = [1]
     ant_weight = 6
-    ant_nbr = 1000
+    ant_nbr = 10000
     ant_list = []
     day = 0
 
@@ -43,30 +43,34 @@ def main():
         ant_list.append(Basic_ant(True, day, "oeuf"))
 
     def canEat():#une colonie mangera la moitié du poid de la colonie
-        need2eat = ant_nbr * ant_weight / 2
-        if need2eat < food_stock:
-            return food_stock - need2eat
-        else:
-            for x in range((need2eat - food_stock) / ant_weight * 2):
+        return ant_nbr * ant_weight / 2
 
-                if ant_nbr > 1:
-                    ant_nbr = ant_nbr - 1
-                    ant_list.pop(1)
-                elif ant_nbr == 1:
-                    ant_list.pop(0)
-                else:
-                    print("Game Over")
 
     def getFood():
-        stocknourriture = (ant_nbr * ant_weight) * 0.25
+        return (ant_nbr * ant_weight) * 0.2
 
+    def killant(nbrdeath):
+        for ant in range(round(nbrdeath)) :
+            if len(ant_list) > 1:
+                ant_list.pop(1)
 
     def affichage():
-        print(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n Nourriture de la colonie : {food_stock}")
+        print(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n Nourriture de la colonie : {food_stock}\n nourriture consomée : {consumed_food}")
 
-    while ant_nbr > 0:
+    while True:
         day = day + 1
-        canEat()
-        getFood()
+        consumed_food = canEat()
+        if food_stock - consumed_food < 0:
+            ant_nbr = ant_nbr - (food_stock - consumed_food)*ant_weight/2 * -1
+            nbrdeadant = (food_stock - consumed_food)*ant_weight/2 * -1
+            killant(nbrdeadant)
+        food_stock = food_stock - consumed_food
+        if food_stock < 0:
+            food_stock = 0
+        food_stock = food_stock + getFood()
         affichage()
+        if ant_nbr < 0:
+            break
+
+
 main()

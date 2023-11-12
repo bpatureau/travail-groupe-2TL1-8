ce32253f51281a59ebfb6a2eea12456f18efe18b
Bastien Patureau <71067765+bpatureau@users.noreply.github.com>
2023-11-12 15:44:32 +0100

    brith of ant

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbrBirth):
             self.birth = birth
             self.phase = phase
 
-        def description(self):
-            return f"classe générique de fourmis."
-
         def __str__(self):
-            return self.description
+            return "classe générique de fourmis."
 
         def isGonnaDie(self, counter):
             if (counter - self.birth >= 10):
@@ -33,40 +30,45 @@ def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbrBirth):
 #        return super().__str__ + f""
 
     ant_list.append(Basic_ant(True, day, "oeuf"))  #reine à devenir
-    for x in range(ant_nbr) :
+    for x in range(ant_nbr):
         ant_list.append(Basic_ant(True, day, "oeuf"))
 
     def manageFoodStock():
-        nourritureRamenee = ant_nbr * 2.8
+        nourritureRamenee = ant_nbr * 2.6
         nourritureConsommee = (ant_nbr * ant_weight) / 2
         return (nourritureConsommee, nourritureRamenee)
 
-    def manageAntNbr(nbrdeath):
+    def manageAntTable(nbrdeath):
+        for ant in range(round(nbrBirth)):
+            ant_list.append(Basic_ant(True, day, "oeuf"))
         for ant in range(round(nbrdeath)):
             if len(ant_list) > 1:
                 ant_list.pop(1)
-        for ant in range(round(nbrBirth)):
-            ant_list.append(Basic_ant(True, day, "oeuf"))
+            else:
+                ant_list.clear()
+
+
     def affichage():
         print(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n Nourriture de la colonie : {food_stock}\n nourriture consommée : {consumed_food}")
 
     while True:
+        nbrdeadant = 0
         day = day + 1
         consumed_food, brought_food = manageFoodStock()
         food_stock = food_stock + brought_food
         if food_stock - consumed_food < 0:
-            ant_nbr = ant_nbr - (food_stock - consumed_food)*ant_weight/2 * -1
             nbrdeadant = (food_stock - consumed_food)*ant_weight/2 * -1
-            manageAntNbr(nbrdeadant)
+        manageAntTable(nbrdeadant)
+        ant_nbr = len(ant_list)
         food_stock = food_stock - consumed_food
         if food_stock < 0:
             food_stock = 0
-        ant_nbr = round(ant_nbr)
         if ant_nbr <= 0:
             ant_nbr = 0
             affichage()
             print("Toutes les fourmis sont mortes")
             break
-        affichage()
+        else:
+            affichage()
 
-main(11000, 1, 6, 100, 1)
+main(1100, 1, 6, 100, 2)

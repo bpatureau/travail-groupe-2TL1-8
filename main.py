7aa07ed43af205f9c62e3e66f089b2a93cad44e4
Bastien Patureau <71067765+bpatureau@users.noreply.github.com>
2023-11-12 14:57:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-
-
-
 # main
 
-def main():
+def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbrBirth):
 # définition globales
 
-    food_stock = 10000000
-    queen_nbr = [1]
-    ant_weight = 6
-    ant_nbr = 10000
     ant_list = []
     day = 0
 
@@ -38,39 +31,42 @@ def main():
 
 #     def __str__(self):
 #        return super().__str__ + f""
+
     ant_list.append(Basic_ant(True, day, "oeuf"))  #reine à devenir
     for x in range(ant_nbr) :
         ant_list.append(Basic_ant(True, day, "oeuf"))
 
-    def canEat():#une colonie mangera la moitié du poid de la colonie
-        return ant_nbr * ant_weight / 2
+    def manageFoodStock():
+        nourritureRamenee = ant_nbr * 2.8
+        nourritureConsommee = (ant_nbr * ant_weight) / 2
+        return (nourritureConsommee, nourritureRamenee)
 
-
-    def getFood():
-        return (ant_nbr * ant_weight) * 0.2
-
-    def killant(nbrdeath):
-        for ant in range(round(nbrdeath)) :
+    def manageAntNbr(nbrdeath):
+        for ant in range(round(nbrdeath)):
             if len(ant_list) > 1:
                 ant_list.pop(1)
-
+        for ant in range(round(nbrBirth)):
+            ant_list.append(Basic_ant(True, day, "oeuf"))
     def affichage():
-        print(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n Nourriture de la colonie : {food_stock}\n nourriture consomée : {consumed_food}")
+        print(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n Nourriture de la colonie : {food_stock}\n nourriture consommée : {consumed_food}")
 
     while True:
         day = day + 1
-        consumed_food = canEat()
+        consumed_food, brought_food = manageFoodStock()
+        food_stock = food_stock + brought_food
         if food_stock - consumed_food < 0:
             ant_nbr = ant_nbr - (food_stock - consumed_food)*ant_weight/2 * -1
             nbrdeadant = (food_stock - consumed_food)*ant_weight/2 * -1
-            killant(nbrdeadant)
+            manageAntNbr(nbrdeadant)
         food_stock = food_stock - consumed_food
         if food_stock < 0:
             food_stock = 0
-        food_stock = food_stock + getFood()
-        affichage()
-        if ant_nbr < 0:
+        ant_nbr = round(ant_nbr)
+        if ant_nbr <= 0:
+            ant_nbr = 0
+            affichage()
+            print("Toutes les fourmis sont mortes")
             break
+        affichage()
 
-
-main()
+main(11000, 1, 6, 100, 1)

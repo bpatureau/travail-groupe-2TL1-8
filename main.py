91fb4d8982fa6a3c2297523f44341c4ade262d49
Bastien Patureau <71067765+bpatureau@users.noreply.github.com>
2023-11-16 15:23:56 +0100

    prettier

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 from random import random
 
 
-def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbrBirth):
-# définition globales
+def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbr_birth):
+    # définition globales
     ant_list = []
     day = 0
 
     # définition de classe
 
-    class Basic_ant:
+    class BasicAnt:
         def __init__(self, isalive, birth, phase):
             self.isAlive = isalive
             self.birth = birth
@@ -18,61 +18,63 @@ def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbrBirth):
         def __str__(self):
             return "classe générique de fourmis."
 
-        def isGonnaDie(self, counter):
-            if (counter - self.birth >= 10):
+        def is_gonna_die(self, counter):
+            if counter - self.birth >= 10:
                 self.isAlive = False
-    class Queen_ant(Basic_ant):
+
+    class QueenAnt(BasicAnt):
         def __init__(self, isalive, birth, phase):
-         super().__init__(isalive, birth, phase)
+            super().__init__(isalive, birth, phase)
 
         def __str__(self):
             return f"Reine"
 
     for queen in range(queen_nbr):
-        ant_list.append(Queen_ant(True, 10, "adulte"))  #reine à devenir
+        ant_list.append(QueenAnt(True, 10, "adulte"))  # reine à devenir
 
     for x in range(ant_nbr):
-        ant_list.append(Basic_ant(True, day, "oeuf"))
+        ant_list.append(BasicAnt(True, day, "oeuf"))
 
-    def manageFoodStock():
-        nourritureRamenee = ant_nbr * (random() + 1) * (foodmodifier * 2)
-        nourritureConsommee = (ant_nbr * ant_weight) / 2
-        return (nourritureConsommee, nourritureRamenee)
-    def genRandomDeath():
+    def manage_food_stock():
+        nourriture_ramenee = ant_nbr * (random() + 1) * (foodmodifier * 2)
+        nourriture_consommee = (ant_nbr * ant_weight) / 2
+        return nourriture_consommee, nourriture_ramenee
+
+    def gen_random_death():
         generatedrandomdeath = round((ant_nbr * (pow(2, random()) - 1) * 75 / 100))
-        print(generatedrandomdeath)
         return generatedrandomdeath
-    def manageAntTable(nbrdeath):
-        nbrQueen = 0
+
+    def manage_ant_table(nbrdeath):
+        nbr_queen = 0
         for ant in ant_list:
-            if isinstance(ant, Queen_ant):
-                nbrQueen += 1
-        for ant in range(round(nbrBirth)):
-            for queen in range(nbrQueen):
-                ant_list.append(Basic_ant(True, day, "oeuf"))
+            if isinstance(ant, QueenAnt):
+                nbr_queen += 1
+        for ant in range(round(nbr_birth)):
+            for queen in range(nbr_queen):
+                ant_list.append(BasicAnt(True, day, "oeuf"))
         for ant in range(round(nbrdeath)):
             if len(ant_list) > 1:
                 ant_list.pop(1)
             else:
                 ant_list.clear()
 
-
-    def affichage():
-        print(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n Nourriture de la colonie : {round(food_stock, 2)}g\n Nourriture consommée : {consumed_food}\n{displayant}\n{displayfood}")
+    def affichage(message):
+        print(message)
+        input("press enter to continue")
 
     while True:
-        input("press enter to continue")
+
         foodmodifier = 1
         displayant = ""
         displayfood = ""
         nbrdeadant = 0
         day = day + 1
-        consumed_food, brought_food = manageFoodStock()
+        consumed_food, brought_food = manage_food_stock()
         food_stock = food_stock + brought_food
         if food_stock - consumed_food < 0:
-            nbrdeadant = (food_stock - consumed_food)*ant_weight/2 * -1
-        nbrdeadant += round(genRandomDeath())
-        manageAntTable(nbrdeadant)
+            nbrdeadant = (food_stock - consumed_food) * ant_weight / 2 * -1
+        nbrdeadant += round(gen_random_death())
+        manage_ant_table(nbrdeadant)
         ant_nbr = len(ant_list)
         food_stock = food_stock - consumed_food
         displayantfactor = round(ant_nbr / 2000 * 10)
@@ -85,10 +87,10 @@ def main(food_stock, queen_nbr, ant_weight, ant_nbr, nbrBirth):
             food_stock = 0
         if ant_nbr <= 0:
             ant_nbr = 0
-            affichage()
-            print("Toutes les fourmis sont mortes")
+            affichage("Toutes les fourmis sont mortes")
             break
         else:
-            affichage()
-
+            affichage(f"Jour : {day} \nFourmis dans la colonie : {ant_nbr} \n"
+            f"Nourriture de la colonie : {round(food_stock, 2)}g\nNourriture consommée : {consumed_food}\n"
+            f"{displayant}\n{displayfood}")
 main(2000, 1, 6, 100, 300)

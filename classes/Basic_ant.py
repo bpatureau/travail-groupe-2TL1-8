840daef6647b4f97db4aeadd9bfb7b9eeb0fa93c
Jerome Verkyndt <92804246+JeromeVerkyndt@users.noreply.github.com>
2024-01-08 15:53:22 +0100

    Update Basic_ant.py
    encap _isAlive

--- classes/Basic_ant.py
+++ classes/Basic_ant.py
@@ -1,12 +1,16 @@
 class Basic_ant:
     def __init__(self, isalive, birth, phase):
-        self.isAlive = isalive
+        self._isAlive = isalive
         self.birth = birth
         self.phase = phase
+
+    @property
+    def get_isAlive(self):
+        return self._isAlive
 
     def __str__(self):
         return "classe générique de fourmis."
 
     def isGonnaDie(self, day):
         if day - self.birth >= 10:
-            self.isAlive = False
+            self._isAlive = False
